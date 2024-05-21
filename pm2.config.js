e84b598ba61ee6816c112f6fa2d149a6030e596b
plehegar <plh@w3.org>
2024-05-21 13:34:07 -0400

    4000 was too much. Document the option as well

--- pm2.config.js
+++ pm2.config.js
@@ -2,6 +2,12 @@ module.exports = {
   /**
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   *
+   * --max-old-space-size=SIZE (in megabytes)
+   *    As memory consumption approaches the limit, V8 will
+   *    spend more time on garbage collection in an effort
+   *    to free unused memory.
+   * https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
    */
   apps: [
     {
@@ -13,7 +19,7 @@ module.exports = {
       },
       error_file: "/var/log/nodejs/github-cache.err",
       out_file: "/var/log/nodejs/github-cache.log",
-      "node_args": "--max_old_space_size=4000"
+      "node_args": "--max_old_space_size=2000"
     }
   ]
 };
